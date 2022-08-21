79864590fda80e06077dca52fb867b2013f0530d
netes <toygame8888@gmail.com>
2022-08-21 08:54:26 +0300

    delegated transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -8,8 +8,11 @@ contract DappToken {
     uint256 public totalSupply;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
+    event Approval(address indexed _owner, address indexed _spender,uint256 _value);
+    
+    
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply){
         balanceOf[msg.sender] = _initialSupply;
@@ -24,5 +27,25 @@ contract DappToken {
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
-
+    
+    // delegate transfer
+    function approve(address _spender, uint256 _value) public returns (bool success){
+       allowance[msg.sender][_spender] = _value;
+       emit Approval(msg.sender, _spender, _value);
+       return true;
+    }
+    
+    // three address are used in transferFrom, the one who call the function itself, the address _from and the address _to
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        
+        allowance[_from][msg.sender] -= _value;
+        
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 }
